7e594566affe4fa1d788c122deffd6bae1f36588
borninfreedom <z19040042@s.upc.edu.cn>
2020-11-02 22:52:25 +0800

    2020年 11月 02日 星期一 22:52:25 CST

---
+++ install_dependancy.sh
@@ -0,0 +1,11 @@
+pip install gym-panda
+# install spinningup
+sudo apt update && sudp apt install -y libopenmpi-dev
+sudo apt install -y git
+git clone https://github.com/mahyaret/spinningup.git
+cd spinningup
+pip install -e .
+
+pip install matplotlib
+pip install numpy
+pip install opencv-python
---
+++ spinup_ppo_panda.py
@@ -0,0 +1,55 @@
+from spinup import ppo_pytorch as ppo
+import torch.nn as nn
+import gym
+import gym_panda
+
+import matplotlib.pyplot as plt
+import numpy as np
+import cv2
+import spinup.algos.pytorch.ppo.core as core
+
+class ProcessFrame84(gym.ObservationWrapper):
+    def __init__(self,env=None):
+        super(ProcessFrame84,self).__init__(env)
+        self.env=env
+        self.observation_space=gym.spaces.Box(
+            low=0,high=255,shape=(84,84,1),dtype=np.uint8
+        )
+
+    def observation(self, observation):
+        return ProcessFrame84.process(self.env.render(mode='rgb_array'))
+
+    def process(frame):
+        if frame.size==720*960*3:
+            img=np.reshape(frame,[720,960,3]).astype(np.float32)
+        else:
+            assert False,"Unknown resolution."
+        img=img[:,:,0]*0.399+img[:,:,1]*0.587+img[:,:,2]*0.114
+        resized_screen=cv2.resize(img,(112,84),interpolation=cv2.INTER_AREA)
+        y_t=resized_screen[:,14:98]
+        y_t=np.reshape(y_t,[84,84,1])
+        return y_t.astype(np.uint8)
+
+
+class ImageToPytorch(gym.ObservationWrapper):
+    def __init__(self,env):
+        super(ImageToPytorch,self).__init__(env)
+        old_shape=self.observation_space.shape
+        new_shape=(old_shape[-1],old_shape[0],old_shape[1])
+        self.observation_space=gym.spaces.Box(
+            low=0.0,high=1.0,shape=new_shape,dtype=np.float32
+        )
+
+    def observation(self, observation):
+        return np.moveaxis(observation,2,0)
+    
+
+class MoveTowardZ(gym.ActionWrapper):
+    pass
+
+env=gym.make('panda-v0')
+env=ProcessFrame84(env)
+env=ImageToPytorch(env)
+env=MoveTowardZ(env)
+
+
